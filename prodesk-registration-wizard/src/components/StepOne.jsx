63c9247dc397d47b0d06e06850a69fa47fbfed28
Aryan214G <aryan214gm@gmail.com>
2026-06-19 16:44:18 +0530

    Add StepOne and StepTwo components with basic structure; update CSS for text color

---
+++ prodesk-registration-wizard/src/components/StepOne.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const StepOne = (props) => {
+
+const { formData, setFormData, setStep } = props;
+
+  return (
+    <div>StepOne</div>
+  )
+}
+
+export default StepOne
---
+++ prodesk-registration-wizard/src/components/StepTwo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const StepTwo = (props) => {
+
+const { formData, setFormData, setStep } = props;
+
+  return (
+    <div>StepTwo</div>
+  )
+}
+
+export default StepTwo
