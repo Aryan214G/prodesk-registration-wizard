73373ff0fb7c1c223e17249747a64d622ac30afc
Aryan214G <aryan214gm@gmail.com>
2026-06-19 19:03:00 +0530

    Implement navigation buttons in StepTwo component

--- prodesk-registration-wizard/src/components/StepTwo.jsx
+++ prodesk-registration-wizard/src/components/StepTwo.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import StepOne from './StepOne';
 
 const StepTwo = (props) => {
 
 const { formData, setFormData, setStep } = props;
 
   return (
-    <div>StepTwo</div>
+    <div>
+        <button
+        onClick={(event) => setStep(1)}>
+            Back
+        </button>
+        <button>
+            Next
+        </button>
+    </div>
   )
 }
 
