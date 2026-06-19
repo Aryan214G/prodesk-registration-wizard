0a7e71da96d6a26f102b0dd74d1476041b6f1c4b
Aryan214G <aryan214gm@gmail.com>
2026-06-19 17:26:14 +0530

    Enhance StepOne component with personal information form and navigation to StepTwo

--- prodesk-registration-wizard/src/components/StepOne.jsx
+++ prodesk-registration-wizard/src/components/StepOne.jsx
@@ -5,7 +5,55 @@ const StepOne = (props) => {
 const { formData, setFormData, setStep } = props;
 
   return (
-    <div>StepOne</div>
+    <div>
+        <h1>Personal Info</h1>
+
+        <div className='form-card'>
+
+            <input
+            type='text'
+            placeholder='Enter first name'
+            value={formData.firstName}
+            onChange={(event) => 
+                setFormData({
+                    ...formData,
+                    firstName: event.target.value 
+                })
+            }
+            />
+
+            <input
+            type='text'
+            placeholder='Enter last name'
+            value={formData.lastName}
+            onChange={(event) => 
+                setFormData({
+                    ...formData,
+                    lastName: event.target.value
+                })
+            }
+            />
+
+            <input
+            type='text'
+            placeholder='Enter date of birth'
+            value={formData.dob}
+            onChange={(event) => 
+                setFormData({
+                    ...formData,
+                    dob: event.target.value
+                })
+            }
+            />
+            
+        </div>
+
+        <button
+        onClick={(event) => setStep(2)}
+        > Next 
+        </button>
+
+    </div>
   )
 }
 
